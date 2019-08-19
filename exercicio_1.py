2b9c0d8e30285b4562f33aa0da0d92803432c335
Vanessa da Paixao <vanessadapaixaobarros1@gmail.com>
2019-08-19 16:41:44 -0300

    Adicionar Atividades do dia

---
+++ exercicio_1.py
@@ -0,0 +1,8 @@
+listas = [ 21,87,46,98,104,53,76,19]
+
+for lista in listas:
+    if lista % 2 == 0:
+        print("O número " + str(lista) + " é par")
+
+    else:
+        print("O número " + str(lista) + " é impar")
